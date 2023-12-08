8dfd732eeb00f37d63212cebd841a6526919f3c2
Joseph Griego <joseph.j.griego@gmail.com>
2023-12-07 19:45:57 -0500

    tiny style tweak

--- 2023/src/07.pl
+++ 2023/src/07.pl
@@ -18,9 +18,6 @@ summarize(Hands, X) :-
 classify_hand(hand(Ranks, Bid), hand(HandClass, Ranks, Bid)) :- 
     hand_structure(Structure, HandClass), \+ \+ permutation(Structure, Ranks), !.
 
-demote_jokers(hand(_, Ranks, _)) :- maplist(demote_joker, Ranks).
-demote_joker(Rank) :- ignore(Rank = 0).
-
 hand_structure([A, A, A, A, A], 900).
 hand_structure([A, A, A, A, _], 800).
 hand_structure([A, A, A, B, B], 700).
@@ -28,6 +25,9 @@ hand_structure([A, A, A, _, _], 600).
 hand_structure([A, A, B, B, _], 500).
 hand_structure([A, A, _, _, _], 400).
 hand_structure([_, _, _, _, _], 100).
+
+demote_jokers(hand(_, Ranks, _)) :- maplist(demote_joker, Ranks).
+demote_joker(Rank) :- ignore(Rank = 0).
 
 input(Jokers, [hand(Ranks, Bid) | More]) --> hand(Jokers, Ranks, Bid), !, eol, input(Jokers, More).
 input(_, []) --> eol.
